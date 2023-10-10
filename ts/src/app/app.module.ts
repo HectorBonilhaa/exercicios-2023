7452c20f4a6bc66d98d1d7e117226a0c2282ca22
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-10 11:04:49 -0300

    :sparkles: container-top

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
